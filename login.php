e9366c149c4d5f6ea8ab782281a136d49125f4d0
joseph cruz galaviz <josephcruzgalaviz20@gmail.com>
2023-05-12 01:04:23 -0600

    subiendo repositorio3

--- login.php
+++ login.php
@@ -1,36 +1,43 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
-    header('HTTP/1.1 405 Method Not Allowed');
-    exit('Method Not Allowed');
-}
 session_start();
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    header('HTTP/1.1 405 Method Not Allowed');
+    exit(json_encode(['error' => 'Method Not Allowed']));
+}
+
 if (isset($_POST['username']) && isset($_POST['password'])) {
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
 
-	$host = 'localhost';
-	$user = 'root';
-	$pass = 'root';
-	$db = 'users';
+    // Aquí debes configurar los detalles de tu base de datos
+    $host = 'localhost';
+    $user = 'root';
+    $pass = 'password';
+    $db = 'database';
 
-	$conn = new mysqli($host, $user, $pass, $db);
+    $conn = new mysqli($host, $user, $pass, $db);
 
-	if ($conn->connect_error) {
-		die('Connection failed: ' . $conn->connect_error);
-	}
+    if ($conn->connect_error) {
+        header('HTTP/1.1 500 Internal Server Error');
+        exit(json_encode(['error' => 'Database connection failed']));
+    }
 
-	$sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
-	$result = $conn->query($sql);
+    $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+    $result = $conn->query($sql);
 
-	if ($result->num_rows == 1) {
-		$_SESSION['username'] = $username;
-		echo 'success';
-	} else {
-		echo 'error';
-	}
+    if ($result->num_rows == 1) {
+        $_SESSION['username'] = $username;
+        exit(json_encode(['success' => true]));
+    } else {
+        header('HTTP/1.1 401 Unauthorized');
+        exit(json_encode(['error' => 'Invalid login credentials']));
+    }
 
-	$conn->close();
+    $conn->close();
+} else {
+    header('HTTP/1.1 400 Bad Request');
+    exit(json_encode(['error' => 'Missing username or password']));
 }
 ?>
 
